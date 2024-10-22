d1466abdc94c47f77dadda16d2aafa3e08977ccc
Andrea <113476650+And-min@users.noreply.github.com>
2024-10-21 18:13:20 -0600

    Add files via upload

---
+++ pin_definition.ino
@@ -0,0 +1,22 @@
+// Pines sensores ultrasonicos
+#define US_R
+#define US_L
+#define US_C
+#define US_G
+
+//Pines motores lado derecho 
+#define FRM1 24
+#define FRM2 22 
+#define PWM_FRM 10  
+#define BRM1 28
+#define BRM2 26
+#define PWM_BRM 11
+
+//Pines motores lado izquierdo 
+#define FLM1 30
+#define FLM2 32
+#define PWM_FLM 12
+#define BLM1 34
+#define BLM2 36
+#define PWM_BLM 13
+
