6db39e299417f5b241762f8fd7adf83777146e70
CCCCC11111 <98575890+CCCCC11111@users.noreply.github.com>
2024-10-22 17:50:51 -0600

    Update sketch_oct14a.ino

--- sketch_oct14a.ino
+++ sketch_oct14a.ino
@@ -1,15 +1,19 @@
-int sensor = A0;
+int Encoder1 = A0;
+int Encoder2= A1;
 bool FRForward =true;
 bool FLForward = true;
 bool BRForward =true;
 bool BLForward = true;
-bool state;
-bool pstate;
+bool stateE1;
+bool pstateE1;
+bool stateE2;
+bool pstateE2;
 int FRcounter;
 int FLcounter;
 int BRcounter;
 int BLcounter;
-int v;
+int E1;
+int E2;
 int vxn;
 int vyn;
 float power;
@@ -50,50 +54,31 @@ void setup()
   pinMode(END, OUTPUT);  // PWM para Motor 4
 
 Serial.begin (9600);
-pinMode(sensor, INPUT);
+pinMode(Encoder1, INPUT);
+pinMode(Encoder2, INPUT);
 }
 
 
 void encoderea(){
-  v= analogRead(sensor);
-      if(v<127){
-        state=false;
+  E1= analogRead(Encoder1);
+  E2= analogRead(Encoder2);
+Serial.print(BRcounter);  Serial.print("\t"); Serial.println(BLcounter);
+//Serial.print(E1); Serial.print("\t"); Serial.println(E2);
+      stateE1 = E1 < 500 ;
+      if(pstateE1!=stateE1){
+        pstateE1=stateE1;
+        FRcounter = FRForward? FRcounter+1: FRcounter-1;
+        BLcounter = BLForward? BLcounter+1: BLcounter-1;
+    
       }
-      else{
-        state=true;
-      }
-      if(pstate!=state){
-        pstate=state;
-
-      if(FRForward){
-        Serial.println(FRcounter);
-        FRcounter++;
-      }
-      else{
-        Serial.println(FRcounter);
-        FRcounter--;
-      }
-      if(FLForward){
-        FLcounter++;
-      }
-      else{
-        FLcounter--;
-      }
-      if(BRForward){
-        BRcounter++;
-      }
-      else{
-        BRcounter--;
-      }
-      if(BLForward){
-        BLcounter++;
-      }
-      else{
-        BLcounter--;
-      }
-
-      }
-
+      stateE2 = E2 < 500 ;
+      if(pstateE2!=stateE2){
+        pstateE2=stateE2;
+        FLcounter = FLForward? FLcounter+1: FLcounter-1;
+        BRcounter = BRForward? BRcounter+1: BRcounter-1;
+      //Serial.print(stateE1); 
+     }
+    
 }
 void FRMover(int v){
   if(v>0){
@@ -102,11 +87,14 @@ void FRMover(int v){
     digitalWrite(IN2, LOW);
     analogWrite(ENA, v);
   }
-  else{
+  else if(v <0){
     FRForward =false;
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     analogWrite(ENA, -v);
+  }
+    else{
+     analogWrite(ENA, 0);
   }
 }
 void BRMover(int v){
@@ -116,11 +104,14 @@ void BRMover(int v){
     digitalWrite(IN4, LOW);
     analogWrite(ENB, v);
   }
-  else{
+  else if(v < 0){
     BRForward = false;
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
     analogWrite(ENB, -v);
+  }
+    else{
+     analogWrite(ENB, 0);
   }
 }
 void FLMover(int v){
@@ -130,11 +121,14 @@ void FLMover(int v){
     digitalWrite(IN6, LOW);
     analogWrite(ENC, v);
   }
-  else{
+  else if(v < 0){
     FLForward =false;
     digitalWrite(IN5, HIGH);
     digitalWrite(IN6, LOW);
     analogWrite(ENC, -v);
+  }
+    else{
+     analogWrite(ENC, 0);
   }
 }
 void BLMover(int v){
@@ -143,14 +137,17 @@ void BLMover(int v){
     digitalWrite(IN7, HIGH);
     digitalWrite(IN8, LOW);
     analogWrite(END, v);
+   
   }
-  else{
+  else if(v < 0){
     BLForward =false;
     digitalWrite(IN7, HIGH);
     digitalWrite(IN8, LOW);
     analogWrite(END, -v);
   }
-
+  else{
+     analogWrite(END, 0);
+  }
 }
 float trampa(float P){
   if(P>power){
@@ -164,8 +161,6 @@ float trampa(float P){
   return power;
 }
 
-
-
 void moverXY(int vx,int vy, float P) {
     vxn=floor(vx*(510/(abs(vx)+abs(vy))));
     vyn=floor(vy*(510/(abs(vx)+abs(vy))));
@@ -175,14 +170,23 @@ void moverXY(int vx,int vy, float P) {
     BRMover(floor((vxn/2-vyn/2)*P));
     encoderea();
 }
-
 void loop(){
+   
+      encoderea();
       if(BRcounter<500){  
-      moverXY(1,0,1);
+        BRMover(255); 
       }
-      if(BRcounter<1000&&BRcounter>=500){  
-        moverXY(1,-1,1);
-        }
+      else{
+        BRMover(0);
+      }
 
+      if(BLcounter<500){  
+        BLMover(255) ;
+      
+      }
+      else{
+        BLMover(0);
+      }
 
+      
 }
